3010e8d7459027e09bfed695327ca323b1b9c41f
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2023-08-13 17:36:33 +0530

    Add project files.

---
+++ src/AudioVisualizer.cpp
@@ -0,0 +1,10 @@
+﻿// AudioVisualizer.cpp : Defines the entry point for the application.
+//
+
+#include "AudioVisualizer.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
